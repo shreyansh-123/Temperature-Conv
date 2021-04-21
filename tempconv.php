de05e97b3fb3be2e7bd272d3418d76da3429d213
shreyansh-123 <54475023+shreyansh-123@users.noreply.github.com>
2021-04-21 15:17:12 +0530

    Add files via upload

---
+++ tempconv.php
@@ -0,0 +1,26 @@
+<html>
+<head>
+    <body>
+    <header>
+        <h1>Temperature Conversion</h1>
+        <div class="Left">
+        
+        </div>
+        <div class="Right">
+        <form>
+            <input type="text" name="number" placeholder="Enter your number">
+            
+            <div class="selection">
+            <label>Celc</label>
+                <input type="radio" name="celc" value="celc">
+                
+                <label>Fehr</label>
+                <input type="radio" name="fehr" value="fehr">
+            </div>
+            <input type="submit" name="submit" value="Convert">
+            </form>
+        </div>
+        </header>
+    </body>
+    </head>
+</html>
